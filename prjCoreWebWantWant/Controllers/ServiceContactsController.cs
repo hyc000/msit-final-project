8564c9198c68ffec9c54e675790a52210d9223c9
M <yentzuchang1990@gmail.com>
2023-08-19 13:23:22 +0800

    待處理訊息列表

--- prjCoreWebWantWant/Controllers/ServiceContactsController.cs
+++ prjCoreWebWantWant/Controllers/ServiceContactsController.cs
@@ -5,7 +5,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using prjCoreWantMember.ViewModels;
 using prjCoreWebWantWant.Models;
+using prjCoreWebWantWant.ViewModels;
 
 namespace prjCoreWebWantWant.Controllers
 {
@@ -18,149 +20,25 @@ namespace prjCoreWebWantWant.Controllers
             _context = context;
         }
 
-        // GET: ServiceContacts
-        public IActionResult Index()
+		public IActionResult List(CKeywordViewModel vm)
+		{
+			NewIspanProjectContext db = new NewIspanProjectContext();
+			IEnumerable<ServiceContact> datas = null;
+			if (string.IsNullOrEmpty(vm.txtKeyword))
+				datas = from s in db.ServiceContacts
+						select s;
+			else
+				datas = db.ServiceContacts.Where(s => s.Account.Name.ToUpper().Contains(vm.txtKeyword.ToUpper())
+					|| s.Account.PhoneNo.ToUpper().Contains(vm.txtKeyword.ToUpper())
+					|| s.Account.Email.ToUpper().Contains(vm.txtKeyword.ToUpper()));
+
+			return View(datas);
+		}
+
+		public IActionResult Index()
         {           
             return View();
         }
-
-        // GET: ServiceContacts/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null || _context.ServiceContacts == null)
-            {
-                return NotFound();
-            }
-
-            var serviceContact = await _context.ServiceContacts
-                .Include(s => s.Account)
-                .FirstOrDefaultAsync(m => m.ServiceContactId == id);
-            if (serviceContact == null)
-            {
-                return NotFound();
-            }
-
-            return View(serviceContact);
-        }
-
-        // GET: ServiceContacts/Create
-        public IActionResult Create()
-        {
-            ViewData["AccountId"] = new SelectList(_context.MemberAccounts, "AccountId", "AccountId");
-            return View();
-        }
-
-        // POST: ServiceContacts/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ServiceContactId,AccountId,ComplaintTitle,ComplaintContent,ProcessStatus")] ServiceContact serviceContact)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(serviceContact);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AccountId"] = new SelectList(_context.MemberAccounts, "AccountId", "AccountId", serviceContact.AccountId);
-            return View(serviceContact);
-        }
-
-        // GET: ServiceContacts/Edit/5
-        public async Task<IActionResult> Edit(int? id)
-        {
-            if (id == null || _context.ServiceContacts == null)
-            {
-                return NotFound();
-            }
-
-            var serviceContact = await _context.ServiceContacts.FindAsync(id);
-            if (serviceContact == null)
-            {
-                return NotFound();
-            }
-            ViewData["AccountId"] = new SelectList(_context.MemberAccounts, "AccountId", "AccountId", serviceContact.AccountId);
-            return View(serviceContact);
-        }
-
-        // POST: ServiceContacts/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ServiceContactId,AccountId,ComplaintTitle,ComplaintContent,ProcessStatus")] ServiceContact serviceContact)
-        {
-            if (id != serviceContact.ServiceContactId)
-            {
-                return NotFound();
-            }
-
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    _context.Update(serviceContact);
-                    await _context.SaveChangesAsync();
-                }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!ServiceContactExists(serviceContact.ServiceContactId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
-            }
-            ViewData["AccountId"] = new SelectList(_context.MemberAccounts, "AccountId", "AccountId", serviceContact.AccountId);
-            return View(serviceContact);
-        }
-
-        // GET: ServiceContacts/Delete/5
-        public async Task<IActionResult> Delete(int? id)
-        {
-            if (id == null || _context.ServiceContacts == null)
-            {
-                return NotFound();
-            }
-
-            var serviceContact = await _context.ServiceContacts
-                .Include(s => s.Account)
-                .FirstOrDefaultAsync(m => m.ServiceContactId == id);
-            if (serviceContact == null)
-            {
-                return NotFound();
-            }
-
-            return View(serviceContact);
-        }
-
-        // POST: ServiceContacts/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
-            if (_context.ServiceContacts == null)
-            {
-                return Problem("Entity set 'NewIspanProjectContext.ServiceContacts'  is null.");
-            }
-            var serviceContact = await _context.ServiceContacts.FindAsync(id);
-            if (serviceContact != null)
-            {
-                _context.ServiceContacts.Remove(serviceContact);
-            }
-            
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-
-        private bool ServiceContactExists(int id)
-        {
-          return (_context.ServiceContacts?.Any(e => e.ServiceContactId == id)).GetValueOrDefault();
-        }
+               
     }
 }
---
+++ prjCoreWebWantWant/ViewModels/CServiceContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿using prjCoreWebWantWant.Models;
+
+namespace prjCoreWebWantWant.ViewModels
+{
+    public class CServiceContactViewModel
+    {
+        public int ServiceContactId { get; set; }
+
+        public int AccountId { get; set; }
+
+        public string? ComplaintTitle { get; set; }
+
+        public string? ComplaintContent { get; set; }
+
+        public bool? ProcessStatus { get; set; }
+
+        public MemberAccount memberAccount { get; set; }
+    }
+}
