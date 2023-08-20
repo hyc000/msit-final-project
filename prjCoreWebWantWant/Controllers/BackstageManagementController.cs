20d78b0c4ce0228bd9280e47a2a12ee3fb8c6479
M <yentzuchang1990@gmail.com>
2023-08-20 18:12:12 +0800

    1

--- prjCoreWebWantWant/Controllers/BackstageManagementController.cs
+++ prjCoreWebWantWant/Controllers/BackstageManagementController.cs
@@ -14,29 +14,6 @@ namespace prjCoreWebWantWant.Controllers
             _context = context;
         }
 
-        public IActionResult List(CKeywordViewModel vm)
-        {
-            NewIspanProjectContext db = new NewIspanProjectContext();
-            IEnumerable<MemberAccount> datas = null;
-            if (string.IsNullOrEmpty(vm.txtKeyword))
-                datas = from m in db.MemberAccounts
-                        join l in db.LoginHistories on m.AccountId equals l.AccountId
-                        join ms in db.MemberStatusLists on m.AccountStatus equals ms.AccountId
-                        select new MemberAccount
-                        {
-                            AccountId = m.AccountId,
-                            Name = m.Name,
-                            Email = m.Email,
-                            AccountStatus = m.AccountStatus,
-                        };
-            //else
-            //    datas = db.ServiceContacts.Where(s => s.Account.Name.ToUpper().Contains(vm.txtKeyword.ToUpper())
-            //        || s.Account.PhoneNo.ToUpper().Contains(vm.txtKeyword.ToUpper())
-            //        || s.Account.Email.ToUpper().Contains(vm.txtKeyword.ToUpper()));
-
-            return View(datas);
-        }
-
         public IActionResult Index()
         {
             return View();
