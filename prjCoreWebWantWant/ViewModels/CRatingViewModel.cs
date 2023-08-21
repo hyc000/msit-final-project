15f2cec1271a480070a420486ebd5ae47bff9efb
Eileen <eileen>
2023-08-21 18:20:19 +0800

    08/21 RatingList少存到兩張CS補上

---
+++ prjCoreWebWantWant/ViewModels/CRatingViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace prjCoreWebWantWant.ViewModels
+{
+    public class CRatingViewModel
+    {
+        public IEnumerable<CRatings> ForOtherRatings { get; set; }
+        public IEnumerable<CRatings> MyRatings { get; set; }
+    }
+}
---
+++ prjCoreWebWantWant/ViewModels/CRatings.cs
@@ -0,0 +1,19 @@
+﻿using prjCoreWebWantWant.Models;
+
+namespace prjCoreWebWantWant.ViewModels
+{
+    public class CRatings
+    {
+       public string 被評論者 { get; set; }
+        public string 評論星數 { get; set; }
+        public string 評論內容 { get; set; }
+
+        public DateTime? 評論日期 { get; set; }
+
+        public string 評論者 { get; set; }
+
+
+
+
+    }
+}
