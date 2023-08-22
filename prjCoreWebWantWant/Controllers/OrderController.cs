7751ae47b21a2fe4ac27bb85a62cb6482e48bf7f
Andy <andy2911131@gmail.com>
2023-08-22 22:10:44 +0800

    0822 1010 更新訂單頁搜尋功能

--- prjCoreWebWantWant/Controllers/OrderController.cs
+++ prjCoreWebWantWant/Controllers/OrderController.cs
@@ -8,19 +8,45 @@ namespace prjShop.Controllers
     {
         private readonly NewIspanProjectContext _context;
 
-        public OrderController(NewIspanProjectContext context) 
+        public OrderController(NewIspanProjectContext context)
         {
             _context = context;
         }
-        public IActionResult List()
+        public IActionResult List(int? categorys, DateTime? startDate, DateTime? endDate, int? orderId, string? name)
         {
-            var o = _context.Orders
-                .Include(o => o.OrderDetails)
-                .ThenInclude(od => od.Product)
-             .Include(o => o.Account)
-             .Include(o =>o.Category)
-         
-             .ToList();
+            var query = _context.Orders
+          .Include(o => o.OrderDetails)
+              .ThenInclude(od => od.Product)
+          .Include(o => o.Account)
+          .Include(o => o.Category)
+          .AsQueryable(); // 将查询初始化为可构建的Queryable
+
+            if (categorys.HasValue)
+            {
+                query = query.Where(o => o.CategoryId == categorys);
+            }
+
+            if (startDate.HasValue)
+            {
+                query = query.Where(o => o.CreateTime >= startDate);
+            }
+
+            if (endDate.HasValue)
+            {
+                query = query.Where(o => o.CreateTime <= endDate);
+            }
+
+            if (orderId.HasValue)
+            {
+                query = query.Where(o => o.OrderId == orderId);
+            }
+
+            if (!string.IsNullOrEmpty(name))
+            {
+                query = query.Where(o => o.Account.Name.Contains(name));
+            }
+
+            var filteredOrders = query.ToList();
 
             var cases = _context.TaskLists.ToList();
             var resumes = _context.Resumes.ToList();
@@ -28,7 +54,7 @@ namespace prjShop.Controllers
             ViewBag.Cases = cases.ToDictionary(c => c.CaseId, c => c.TaskTitle);
             ViewBag.Resumes = resumes.ToDictionary(r => r.ResumeId, r => r.ResumeId);
 
-            return View(o);
+            return View(filteredOrders);
         }
     }
 }
