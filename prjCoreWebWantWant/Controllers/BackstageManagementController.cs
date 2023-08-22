87468cd87bdbf892791c31bd6c9e9bef2f9a45ff
M <yentzuchang1990@gmail.com>
2023-08-22 17:31:21 +0800

    0822 1730 update

--- prjCoreWebWantWant/Controllers/BackstageManagementController.cs
+++ prjCoreWebWantWant/Controllers/BackstageManagementController.cs
@@ -15,7 +15,102 @@ namespace prjCoreWebWantWant.Controllers
             _context = context;
         }
 
+        //public IActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //        return RedirectToAction("List");
+        //    NewIspanProjectContext db = new NewIspanProjectContext();
+        //    MemberAccount datas = db.MemberAccounts.FirstOrDefault(m => m.AccountId == id);
+        //    if (datas == null)
+        //        return RedirectToAction("List");
+        //    return View(datas);
+        //}
 
+        //[HttpPost]
+        //public IActionResult Edit(int id, CBackstageManagementViewModel vm)
+        //{
+        //    NewIspanProjectContext db = new NewIspanProjectContext();
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        // 根據 ID 從數據庫中獲取對應的 memberStatusList 記錄
+        //        var memberStatusList = db.MemberStatusLists.FirstOrDefault(ms => ms.AccountId == id);
+
+        //        if (memberStatusList != null)
+        //        {
+        //            // 更新帳號狀態
+        //            memberStatusList.memberAccount.AccountStatus = vm.accountStatus;
+
+        //            // 更新停權原因
+        //            memberStatusList.StatusChangeReason = vm.memberStatusList.StatusChangeReason;
+
+        //            // 儲存變更到數據庫
+        //            db.SaveChanges();
+
+        //            // 重導向到修改後的頁面或其他操作
+        //            return RedirectToAction("List");
+        //        }
+        //    }
+
+        //    // 如果模型無效，返回原始的編輯頁面，顯示錯誤信息
+        //    return View(vm);
+        //}
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return RedirectToAction("List");
+
+            NewIspanProjectContext db = new NewIspanProjectContext();
+            // 獲取相關的數據，例如 memberStatusList, memberAccount 等
+            CBackstageManagementViewModel vm = GetViewModelById(id);
+
+            if (vm == null)
+                return RedirectToAction("List");
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int id, CBackstageManagementViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                NewIspanProjectContext db = new NewIspanProjectContext();
+                // 根據 ID 獲取相關的數據，例如 memberStatusList, memberAccount 等
+                CBackstageManagementViewModel pDb = GetViewModelById(id);
+
+                if (pDb != null)
+                {                   
+                    pDb.Name = vm.Name;
+                    pDb.Email = vm.Email;
+                    pDb.PhoneNo = vm.PhoneNo;
+                    pDb.AccountStatus = vm.AccountStatus;
+                    pDb.memberStatusList.StatusChangeReason = vm.memberStatusList.StatusChangeReason;
+
+                    // 儲存變更到數據庫
+                    db.SaveChanges();
+                }
+
+                return RedirectToAction("List");
+            }
+
+            return View(vm);
+        }
+
+        private CBackstageManagementViewModel GetViewModelById(int? id)
+        {
+            NewIspanProjectContext db = new NewIspanProjectContext();
+           
+            CBackstageManagementViewModel vm = new CBackstageManagementViewModel
+            {
+                //memberAccount = db.MemberAccounts.FirstOrDefault(m => m.AccountId == id),
+                memberStatusList = db.MemberStatusLists.FirstOrDefault(ms => ms.AccountId == id),
+                // 其他相關的數據賦值
+            };
+
+            return vm;
+        }
 
         public IActionResult List(CKeywordViewModel vm)
         {
@@ -61,7 +156,7 @@ namespace prjCoreWebWantWant.Controllers
                              CreateTime = m.CreateTime,
                              AccountStatus = m.AccountStatus,
                              memberStatusList = ms,
-                             loginHistory = lastLoginTime,
+                             loginHistory = lastLoginTime,                             
                          }).AsEnumerable().DistinctBy(vm => vm.AccountId);
 
             else
