093709f20b6a45fc5b42e10765a37fe98e757ab9
Eileen <eileen>
2023-08-23 19:33:41 +0800

    08/23 PM7:32  ExpertTaskList好了!!!!!!

---
+++ prjCoreWebWantWant/Models/CExperTaskFactory.cs
@@ -0,0 +1,45 @@
+﻿using prjCoreWebWantWant.ViewModels;
+
+namespace prjCoreWebWantWant.Models
+{
+    public class CExperTaskFactory
+    {
+        private readonly NewIspanProjectContext _context;
+     
+        public CExperTaskFactory(NewIspanProjectContext context)
+        {
+            _context = context;
+           
+        
+        }
+
+        //Member轉換器 ID變成名字
+        public string MemberName(int? MemberID)
+        {
+            if (MemberID != null)
+            {
+                string memberName = _context.MemberAccounts
+                .Where(x => x.AccountId == MemberID)
+                .Select(x => x.Name)
+                .FirstOrDefault();
+                return memberName;
+            }
+
+            return null;
+        }
+        public string StatusName(int? StatusID)
+        {
+            if (StatusID != null)
+            {
+                string statusName = _context.CaseStatusLists
+                .Where(x => x.CaseStatusId == StatusID)
+                .Select(x => x.CaseStatus)
+                .FirstOrDefault();
+                return statusName;
+            }
+
+            return null;
+        }
+
+    }
+}
---
+++ prjCoreWebWantWant/ViewModels/CExpertTaskListViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace prjCoreWebWantWant.ViewModels
+{
+    public class CExpertTaskListViewModel
+    {
+        public IEnumerable<CExpertTaskViewModel> mytasking { get; set; }
+        public IEnumerable<CExpertTaskViewModel> mytasked { get; set; }
+
+        public IEnumerable<CExpertTaskViewModel> taskfromotherno { get; set; }
+
+        public IEnumerable<CExpertTaskViewModel> taskingfromother { get; set; }
+
+        public IEnumerable<CExpertTaskViewModel> taskedfromother { get; set; }
+
+
+
+    }
+}
