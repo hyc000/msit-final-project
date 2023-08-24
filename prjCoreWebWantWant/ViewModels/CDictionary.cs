6f03ad20858aab1e947a5b993deefb8ee6cb8c9f
hyc000 <81237442+hyc000@users.noreply.github.com>
2023-08-24 11:33:52 +0800

    0824 am11:31 將CDictionary彙整於Models內

--- prjCoreWebWantWant/ViewModels/CDictionary.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace prjCoreWantMember.ViewModels
-{
-    public class CDictionary
-    {
-        public static readonly string SK_TASKDETAIL_LIST = "SK_TASKDETAIL_LIST";
-        public static readonly string SK_LOGINED_USER = "SK_LOGINED_USER";
-    }
-}
