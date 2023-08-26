bb733f02791333e79bee848749ff38d322df3596
unknown <yuchieh.hong@gmail.com>
2023-08-26 14:56:38 +0800

    0826 pm2:55 用壞的api

--- prjCoreWebWantWant/Controllers/ApiForExpertController.cs
+++ prjCoreWebWantWant/Controllers/ApiForExpertController.cs
@@ -38,10 +38,10 @@ namespace prjCoreWebWantWant.Controllers
         public IActionResult SkillAPI(string skilltype)
         {
             int skilltypeID = _db.SkillTypes
-                .Where(x=>x.SkillTypeName == skilltype)
-                .Select(x=>x.SkillTypeId)
+                .Where(x => x.SkillTypeName == skilltype)
+                .Select(x => x.SkillTypeId)
                 .FirstOrDefault();
-           
+
             IEnumerable<string> data = _db.Skills
                 .Where(s=>s.SkillTypeId== skilltypeID)
                .Select(x => x.SkillName)
@@ -58,5 +58,49 @@ namespace prjCoreWebWantWant.Controllers
            
         }
 
+
+        //Certificate Type的API
+
+        public IActionResult CertificateTypeAPI()
+        {
+
+            IEnumerable<string> data = _db.CertificateTypes
+                .Select(x => x.CertificateTypeName)
+                .ToList();
+            if (data != null)
+            {
+                return Json(data);
+            }
+            else
+            {
+                string notsearchdata = "沒有相關資料";
+                return Json(notsearchdata);
+            }
+
+        }
+
+        //Certificate 的API
+        public IActionResult CertificateAPI(string certificatetype)
+        {
+            int certificatetypeID = _db.CertificateTypes
+                .Where(x => x.CertificateTypeName == certificatetype)
+                .Select(x => x.CertificateTypeId)
+                .FirstOrDefault();
+
+            IEnumerable<string> data = _db.Certificates
+                .Where(s => s.CertificateTypeId == certificatetypeID)
+               .Select(x => x.CertificateName)
+               .ToList();
+            if (data != null)
+            {
+                return Json(data);
+            }
+            else
+            {
+                string notsearchdata = "沒有相關資料";
+                return Json(notsearchdata);
+            }
+
+        }
     }
 }
