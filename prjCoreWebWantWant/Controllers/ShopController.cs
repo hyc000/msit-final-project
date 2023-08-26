07786eaaa1cdf311b615a03fd2a48203de6a17fe
Andy <andy2911131@gmail.com>
2023-08-26 23:30:59 +0800

    0826 11:25  購買置頂規則確定

--- prjCoreWebWantWant/Controllers/ShopController.cs
+++ prjCoreWebWantWant/Controllers/ShopController.cs
@@ -210,25 +210,162 @@ namespace prjShop.Controllers
         }
 
 
-        
+
+//        [HttpPost]
+//        public IActionResult Checkout(int? CaseId, Order newOrder, List<OrderDetail> newOrderDetails)
+//        {
+
+
+//            int userId = GetAccountID();
+//            var cart = GetCart(userId);
+
+//            if (cart.Count == 0)
+//            {
+//                TempData["ErrorMessage"] = "購物車內沒有商品，無法進行結帳。";
+//                return RedirectToAction("CaseCart");
+//            }
+
+//            TempData["SweetAlertScript"] = null;
+//            foreach (var cartItem in cart)
+//            {
+//                var product = _context.Products.FirstOrDefault(p => p.ProductId == cartItem.ProductId);
+//                if (product == null || product.UnitsInStock < cartItem.Quantity)
+//                {
+//                    // 使用SweetAlert2顯示提示消息
+//                    string errorMessage = "【" + product.ProductName + "】庫存不足，無法完成結帳。";
+//                    TempData["ErrorMessage"] = errorMessage;
+
+//                    // 使用SweetAlert2的JavaScript代碼顯示警告框
+//                    string sweetAlertScript = $"Swal.fire('庫存不足', '{errorMessage}', 'warning');";
+//                    TempData["SweetAlertScript"] = sweetAlertScript;
+
+//                    return RedirectToAction("CaseCart");
+//                }
+//            }
+
+//            // 創建新的訂單
+//            newOrder.AccountId = userId;
+//            newOrder.PayWayId = 1;
+//            newOrder.CategoryId = 2;
+//            newOrder.CreateTime = DateTime.Now;
+//            _context.Orders.Add(newOrder);
+//            _context.SaveChanges();
+
+//            // 將每個購物車項目關聯到訂單
+//            foreach (var cartItem in cart)
+//            {
+//                var newOrderDetail = new OrderDetail
+//                {
+//                    OrderId = newOrder.OrderId,
+//                    ProductId = cartItem.ProductId,
+//                    UnitPrice = (int)cartItem.UnitPrice,
+//                    Quantity = cartItem.Quantity,
+//                    TopDate = cartItem.TopDate,
+//                    TopType = "任務置頂",
+//                    GetPoint = cartItem.GetPoint,
+//                    CaseId = CaseId,
+
+//                };
+//                newOrderDetails.Add(newOrderDetail);
+//            }
+
+//            // 將所有新的 OrderDetail 加入 _context
+//            _context.OrderDetails.AddRange(newOrderDetails);
+//            _context.SaveChanges();
+
+//            //更新庫存
+//            foreach (var orderDetail in newOrderDetails)
+//            {
+//                var product = _context.Products.FirstOrDefault(p => p.ProductId == orderDetail.ProductId);
+//                if (product != null)
+//                {
+//                    product.UnitsInStock -= orderDetail.Quantity;
+//                    _context.SaveChanges();
+//                }
+//            }
+
+//            // 更新會員點數
+//            int orderGetPoint = Convert.ToInt32(Request.Form["OrderGetPoint"]);
+//            int orderUsePoint = Convert.ToInt32(Request.Form["OrderUsePoint"]);
+
+//            var memberAccount = _context.MemberAccounts.FirstOrDefault(p => p.AccountId == userId);
+//            if (memberAccount != null)
+//            {   // 保證不會扣除超過會員現有的點數
+//                if (orderUsePoint > memberAccount.MemberTotalPoint)
+//                {
+//                    orderUsePoint = memberAccount.MemberTotalPoint; // 將扣除點數調整為會員現有的點數
+//                }
+
+//                memberAccount.MemberTotalPoint -= orderUsePoint;
+//                memberAccount.MemberTotalPoint += orderGetPoint;
+//                _context.SaveChanges();
+//            }
+
+//            // 更新top
+//foreach (var orderDetail in newOrderDetails)
+//            {
+//                var product = _context.Products.FirstOrDefault(p => p.ProductId == orderDetail.ProductId);
+//                if (product != null)
+//                {
+//                    if (orderDetail.TopDate != null) // 只处理不为null的TopDate
+//                    {
+//                        var task = _context.TaskLists.FirstOrDefault(t => t.CaseId == orderDetail.CaseId);
+//                        if (task != null)
+//                        {
+//                            DateTime newOnTop;
+//                            int day = (int)(orderDetail.TopDate * orderDetail.Quantity);
+//                            if (task.OnTop == null || task.OnTop < DateTime.Now)
+//                            {
+                               
+//                                newOnTop = DateTime.Now.AddDays(day);
+//                            }
+//                            else
+//                            {
+//                                newOnTop = task.OnTop.Value.AddDays(day);
+//                            }
+
+//                            task.OnTop = newOnTop;
+
+//                            // 更新数据库中的任务表
+//                            _context.TaskLists.Update(task);
+//                        }
+//                    }
+//                }
+//            }
+
+//           
+//            _context.SaveChanges();
+
+//            // 清空購物車
+//            cart.Clear();
+//            SaveCart(cart, userId);
+
+//            return RedirectToAction("Order");
+//        }
+
         [HttpPost]
         public IActionResult Checkout(int? CaseId, Order newOrder, List<OrderDetail> newOrderDetails)
         {
-           
-
             int userId = GetAccountID();
             var cart = GetCart(userId);
+
+            if (cart.Count == 0)
+            {
+                TempData["ErrorMessage"] = "購物車內沒有商品，無法進行結帳。";
+                return RedirectToAction("CaseCart");
+            }
+
             TempData["SweetAlertScript"] = null;
             foreach (var cartItem in cart)
             {
                 var product = _context.Products.FirstOrDefault(p => p.ProductId == cartItem.ProductId);
                 if (product == null || product.UnitsInStock < cartItem.Quantity)
                 {
-                    // 使用SweetAlert2顯示提示消息
-                    string errorMessage = "【"+product.ProductName+"】庫存不足，無法完成結帳。";
+                    // 使用SweetAlert顯示提示消息
+                    string errorMessage = "【" + product.ProductName + "】庫存不足，無法完成結帳。";
                     TempData["ErrorMessage"] = errorMessage;
 
-                    // 使用SweetAlert2的JavaScript代碼顯示警告框
+                    // 使用SweetAlert的JavaScript代碼顯示警告框
                     string sweetAlertScript = $"Swal.fire('庫存不足', '{errorMessage}', 'warning');";
                     TempData["SweetAlertScript"] = sweetAlertScript;
 
@@ -257,8 +394,23 @@ namespace prjShop.Controllers
                     TopType = "任務置頂",
                     GetPoint = cartItem.GetPoint,
                     CaseId = CaseId,
-
                 };
+
+                // 更新任務的OnTop
+                var task = _context.TaskLists.FirstOrDefault(t => t.CaseId == newOrderDetail.CaseId);
+                if (task != null && newOrderDetail.TopDate.HasValue)
+                {
+                    int day = (int)(newOrderDetail.TopDate * newOrderDetail.Quantity);
+                    if (task.OnTop == null || task.OnTop < DateTime.Now)
+                    {
+                        task.OnTop = DateTime.Now.AddDays(day);
+                    }
+                    else
+                    {
+                        task.OnTop = task.OnTop.Value.AddDays(day);
+                    }
+                }
+
                 newOrderDetails.Add(newOrderDetail);
             }
 
@@ -273,16 +425,33 @@ namespace prjShop.Controllers
                 if (product != null)
                 {
                     product.UnitsInStock -= orderDetail.Quantity;
-                    _context.SaveChanges(); 
                 }
+            }
+
+            // 更新會員點數
+            int orderGetPoint = Convert.ToInt32(Request.Form["OrderGetPoint"]);
+            int orderUsePoint = Convert.ToInt32(Request.Form["OrderUsePoint"]);
+
+            var memberAccount = _context.MemberAccounts.FirstOrDefault(p => p.AccountId == userId);
+            if (memberAccount != null)
+            {   // 保證不會扣除超過會員現有的點數
+                if (orderUsePoint > memberAccount.MemberTotalPoint)
+                {
+                    orderUsePoint = memberAccount.MemberTotalPoint; // 將扣除點數調整為會員現有的點數
+                }
+
+                memberAccount.MemberTotalPoint -= orderUsePoint;
+                memberAccount.MemberTotalPoint += orderGetPoint;
             }
 
             // 清空購物車
             cart.Clear();
             SaveCart(cart, userId);
 
+            
+            _context.SaveChanges();
+
             return RedirectToAction("Order");
         }
-
     }
 }
