356bce58dd1ec8a2c079fd8901465e03db352340
Eileen <shingirl520@gmail.com>
2023-08-28 02:26:39 +0800

    8/28 AM2:25 rating

--- prjCoreWebWantWant/Controllers/RatingsController.cs
+++ prjCoreWebWantWant/Controllers/RatingsController.cs
@@ -32,26 +32,78 @@ namespace prjCoreWebWantWant.Controllers
 
 
         }
-      
-        // GET: Ratings/Create
-        public IActionResult Create(int? id)
+        public IActionResult CreateNew(int? id)//caseid
         {
-            CRatingCreatViewModel vm=new CRatingCreatViewModel();
-            vm.taskprincipal = _context.MemberAccounts
-                .Where(x => x.AccountId == _memberID)
-                .Select(u => u.Name)
-                .FirstOrDefault();
-            vm.taskexperter = _context.MemberAccounts
-                .Where(x => x.AccountId == id)
-                .Select(u => u.Name)
-                .FirstOrDefault();
-
-
+            CExperTaskFactory factory = new CExperTaskFactory(_context);
+            CRatingCreatViewModel vm = new CRatingCreatViewModel();
+            vm.taskid = id.GetValueOrDefault(); ;
+            vm.委託者 = factory.MemberName(_memberID);
+           int expertid= _context.TaskLists
+                .Where(x => x.CaseId == id)
+                .Select(u => u.AccountId)
+                .FirstOrDefault()
+                .GetValueOrDefault(); 
+            vm.專家 = factory.MemberName(expertid);
             return View(vm);
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateNew([Bind("taskid,委託者,專家,評論星數,評論內容")] CRatingCreatViewModel vm)
+        {
+            //if (ModelState.IsValid) 
+            //{
+                CExperTaskFactory factory = new CExperTaskFactory(_context);
+
+                DateTime date = DateTime.Now;
+                Rating rating = new Rating();
+                rating.RatingStar = vm.評論星數;
+                rating.RatingContent = vm.評論內容;
+                rating.RatingDate = date;
+                rating.SourceRoleId = 3;
+                rating.SourceAccountId = factory.MemberID(vm.專家);
+                rating.TargetRoleId = 1;
+                rating.TargetAccountId = factory.MemberID(vm.委託者);
+
+                _context.Add(rating); 
+               _context.SaveChanges();
+                int newratingid= rating.RatingId;
+                
+                ExpertApplication expertapplication = _context.ExpertApplications
+                    .Where(x=>x.CaseId==vm.taskid)
+                    .FirstOrDefault();
+                expertapplication.RatingId = newratingid;
+
+                _context.Update(expertapplication);
+                _context.SaveChanges();
+
+
+                return RedirectToAction("Index");
+            //}
+            //return View();
+
+
+        }
+
+
+        // GET: Ratings/Create
+        //    public IActionResult Create(int? id)
+        //{
+        //CRatingCreatViewModel vm=new CRatingCreatViewModel();
+        //vm.taskprincipal = _context.MemberAccounts
+        //    .Where(x => x.AccountId == _memberID)
+        //    .Select(u => u.Name)
+        //    .FirstOrDefault();
+        //vm.taskexperter = _context.MemberAccounts
+        //    .Where(x => x.AccountId == id)
+        //    .Select(u => u.Name)
+        //    .FirstOrDefault();
+
+
+        //return View(vm);
+        //}
 
         // POST: Ratings/Create
-        
+
         [HttpPost]
         [ValidateAntiForgeryToken]
         public IActionResult Create()
@@ -70,10 +122,10 @@ namespace prjCoreWebWantWant.Controllers
 
         //不用了改回INDEX
 
-        public async Task<IActionResult> RatingList()
-        {
-            return View();
-        }
+        //public async Task<IActionResult> RatingList()
+        //{
+        //    return View();
+        //}
 
     }
 }
--- prjCoreWebWantWant/ViewModels/CRatingCreatViewModel.cs
+++ prjCoreWebWantWant/ViewModels/CRatingCreatViewModel.cs
@@ -2,16 +2,15 @@
 {
     public class CRatingCreatViewModel
     {
-        public string taskprincipal { get; set; }//委託者:
-        public string taskexperter { get; set; }//接案者:
+        public string 委託者 { get; set; }//委託者:
+        public string 專家 { get; set; }//接案者:
 
-        public string taskname { get {
-                string name = $"委託者{taskprincipal}跟接案者{taskexperter}的委託單";
-                return name; } }
-        public int starscore { get; set; }
+        public int taskid { get; set; }
+        public string 評論星數 { get; set; }
 
-        public string ratingcontent { get; set; }
-     
+        public string 評論內容 { get; set; }
+        public string 評論日期 { get; set; }
+
 
     }
 }
