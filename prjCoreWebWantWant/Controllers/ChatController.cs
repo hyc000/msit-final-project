fe1741bb54c41a48d2d69c1ad4d5f4ec3f4f0113
sora2919 <sora2919@gmail.com>
2023-08-27 13:03:39 +0800

    聊天室-只能開新視窗的1:1聊天室

--- prjCoreWebWantWant/Controllers/ChatController.cs
+++ prjCoreWebWantWant/Controllers/ChatController.cs
@@ -1,6 +1,7 @@
 ﻿using DocumentFormat.OpenXml.Office2010.Excel;
 using Microsoft.AspNetCore.Mvc;
 using prjCoreWebWantWant.Models;
+using prjCoreWebWantWant.ViewModels;
 using System.Text.Json;
 
 namespace WantTask.Controllers
@@ -82,10 +83,84 @@ namespace WantTask.Controllers
                 return RedirectToAction("Login", "Member");
         }
 
-        public IActionResult ChatIcon()
+        public IActionResult ChatTo(int id)
         {
+            //if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //判斷是否有登入
+            //{
+            //    string userDataJson = HttpContext.Session.GetString(CDictionary.SK_LOGINED_USER);
+            //    CLoginUser loggedInUser = JsonSerializer.Deserialize<CLoginUser>(userDataJson);
 
-            return PartialView();
+
+            //    //我的頭像
+            //    var myava = _db.MemberAccounts.Where(p => p.AccountId == loggedInUser.AccountId).Select(p => p.MemberPhoto).FirstOrDefault();
+            //    byte[] userAvatarBytes = myava;
+            //    string userAvatarBase64 = Convert.ToBase64String(userAvatarBytes);
+            //    string userAvatarUrl = $"data:image/png;base64,{userAvatarBase64}";
+
+            //    //對方的頭像
+            //    var withava = _db.MemberAccounts.Where(p => p.AccountId == id).Select(p => p.MemberPhoto).FirstOrDefault();
+            //    byte[] withUserAvatarBytes = withava;
+            //    string withUserAvatarBase64 = Convert.ToBase64String(withUserAvatarBytes);
+            //    string withUserAvatarUrl = $"data:image/png;base64,{withUserAvatarBase64}";
+
+            //    var chatModel= new CChatToViewModel
+            //    {
+            //        chatWithId = id,
+            //        currentLoginId = loggedInUser.AccountId,
+            //        currentLoginAvatarUrl = userAvatarUrl,
+            //        chatWithUserAvatarUrl = withUserAvatarUrl
+            //    };
+
+
+            //    return PartialView("ChatTo", chatModel);
+            //}
+            //else
+            //{
+            //    string BackToAction = "ChatSingle"; //目前的action名字
+            //    string BackToController = "Chat";//目前的controller名字
+            //    string BackToId = id.ToString();//目前的id位置
+            //    return RedirectToAction("Login", "Member", new { BackToAction, BackToController, BackToId });
+            //}
+            System.Diagnostics.Debug.WriteLine($"Received id: {id}");
+
+            if (HttpContext.Session.Keys.Contains(CDictionary.SK_LOGINED_USER)) //判斷是否有登入
+            {
+                string userDataJson = HttpContext.Session.GetString(CDictionary.SK_LOGINED_USER);
+                CLoginUser loggedInUser = JsonSerializer.Deserialize<CLoginUser>(userDataJson);
+
+                ViewBag.chatWithId = id;
+                ViewBag.currentLoginId = loggedInUser.AccountId;
+
+                //我的頭像
+                var myava = _db.MemberAccounts.Where(p => p.AccountId == loggedInUser.AccountId).Select(p => p.MemberPhoto).FirstOrDefault();
+                byte[] userAvatarBytes = myava;
+                string userAvatarBase64 = Convert.ToBase64String(userAvatarBytes);
+                string userAvatarUrl = $"data:image/png;base64,{userAvatarBase64}";
+                ViewBag.currentLoginAvatarUrl = userAvatarUrl;
+
+                //對方的頭像
+                var withava = _db.MemberAccounts.Where(p => p.AccountId == id).Select(p => p.MemberPhoto).FirstOrDefault();
+                byte[] withUserAvatarBytes = withava;
+                string withUserAvatarBase64 = Convert.ToBase64String(withUserAvatarBytes);
+                string withUserAvatarUrl = $"data:image/png;base64,{withUserAvatarBase64}";
+                ViewBag.chatWithUserAvatarUrl = withUserAvatarUrl;
+
+                var chatModel = new CChatToViewModel
+                {
+                    chatWithId = id,
+                    currentLoginId = loggedInUser.AccountId,
+                    currentLoginAvatarUrl = userAvatarUrl,
+                    chatWithUserAvatarUrl = withUserAvatarUrl
+                };
+                return PartialView("ChatTo", chatModel);
+            }
+            else
+            {
+                string BackToAction = "ChatTo"; //目前的action名字
+                string BackToController = "Chat";//目前的controller名字
+                string BackToId = id.ToString();//目前的id位置
+                return RedirectToAction("Login", "Member", new { BackToAction, BackToController, BackToId });
+            }
         }
 
         public IActionResult ChatTest(int id)
