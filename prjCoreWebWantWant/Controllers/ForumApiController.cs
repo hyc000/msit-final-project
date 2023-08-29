995c11976bc5912a84e5c109a3f21a5004eb3cf1
sora2919 <sora2919@gmail.com>
2023-08-29 11:02:25 +0800

    聊天室-修改單人聊天時顯示對象名字

--- prjCoreWebWantWant/Controllers/ForumApiController.cs
+++ prjCoreWebWantWant/Controllers/ForumApiController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using prjCoreWebWantWant.ViewModels;
 
 namespace prjCoreWebWantWant.Controllers
 {
     public class ForumApiController : Controller
     {
+        [HttpPost]
         public IActionResult PostReply()
         {
+            ForumPostReply reply = new ForumPostReply();
 
 
             return View();
