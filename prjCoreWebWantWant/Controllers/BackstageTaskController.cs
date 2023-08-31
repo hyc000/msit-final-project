d87202171d9cc80238d31f4081a5aaf4fd6dc975
Alysonpanda <ywljk0518@gmail.com>
2023-08-31 12:50:39 +0800

    0831 1250 一些小廢廢功能

--- prjCoreWebWantWant/Controllers/BackstageTaskController.cs
+++ prjCoreWebWantWant/Controllers/BackstageTaskController.cs
@@ -165,6 +165,18 @@ namespace WantTask.Controllers
             return RedirectToAction("TablesEditable");
         }
 
+        //改狀態為空值=UI的刪除
+        public IActionResult PubNull(int? id)
+        {
+            TaskList task = _context.TaskLists.FirstOrDefault(p => p.CaseId == id);
+            if (id != null)
+            {
+                task.PublishOrNot = "";
+                _context.SaveChanges();
+            }
+            return RedirectToAction("TablesEditable");
+        }
+
 
         public IActionResult Edit(int? id)
         {
