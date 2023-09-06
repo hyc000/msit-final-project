021778de575cb02217d0f13678ecf2e5759c4ce3
Eileen <eileen>
2023-09-06 10:15:24 +0800

    9/6 AM10:15小改

---
+++ prjCoreWebWantWant/Hubs/CExpertTaskInfo.cs
@@ -0,0 +1,38 @@
+﻿namespace prjCoreWebWantWant.Hubs
+{
+    public static class ConnectionManager
+    {
+        private static readonly Dictionary<string, string> _connections = new Dictionary<string, string>();
+
+        public static void AddConnection(string userId, string connectionId)
+        {
+            if (string.IsNullOrEmpty(userId) || string.IsNullOrEmpty(connectionId))
+            {
+                throw new ArgumentException("UserId and ConnectionId cannot be null or empty.");
+            }
+
+            lock (_connections)
+            {
+                _connections[userId] = connectionId;
+            }
+        }
+
+        public static string GetConnectionId(string userId)
+        {
+            lock (_connections)
+            {
+                _connections.TryGetValue(userId, out string connectionId);
+                return connectionId;
+            }
+        }
+
+        public static void RemoveConnection(string userId)
+        {
+            lock (_connections)
+            {
+                _connections.Remove(userId);
+            }
+        }
+    }
+
+}
---
+++ prjCoreWebWantWant/Hubs/CExpertTaskeViewModelHub.cs
@@ -0,0 +1,23 @@
+﻿namespace prjCoreWebWantWant.Hubs
+{
+    public class CExpertTaskeViewModelHub
+    {
+
+        public string taskmember { get; set; }
+        public int taskmemberid { get; set; }
+        public string taskexpert { get; set; }
+        public int taskexpertid { get; set; }
+        public int caseid { get; set; }
+        public string taskcontent { get; set; }
+        public string taskdatestart { get; set; }
+        public string taskdateend { get; set; }
+        public int? taskprice { get; set; }
+        public string CaseStatusname { get; set; }
+
+        public string WorkPlace { get; set; }
+        public string Address { get; set; }
+
+        public string UserB { get; set; }
+        public string divId { get; set; }
+    }
+}
